432fa65146a2d449ac379e0cb187a6275dac5598
mjarkk <mkopenga@gmail.com>
2019-02-06 14:35:21 +0100

    Addes some wallpapers and modified my fish config

--- config.fish
+++ config.fish
@@ -1,6 +1,19 @@
 set -Ux GOPATH $HOME/go
-set -x PATH $GOPATH/bin /opt/android-sdk/platform-tools /home/mark/Android/Sdk/emulator $PATH
+set -x PATH $GOPATH/bin $PATH
 
 function lg
   lazygit $argv
 end
+
+function ss
+  sudo systemctl $argv
+end
+
+function c 
+  code ./
+end
+
+function pacup 
+  sudo pacman -Syuu
+end
+
