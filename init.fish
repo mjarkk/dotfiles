5a8dfcaccc936f57c224ef2967d70109b6558d93
mjarkk <mkopenga@gmail.com>
2023-05-14 16:49:53 +0200

    Comitting changes made to init.fish

--- init.fish
+++ init.fish
@@ -5,63 +5,67 @@ end
 set -x PATH /opt/local/bin $PATH
 set -x PATH /home/mark/.local/bin $PATH
 
+# Common linux things..
+# set -x PATH /home/mark/Documents/flutter/bin $PATH
+# set -Ux CHROME_EXECUTABLE "/home/mark/.local/share/flatpak/exports/bin/com.google.Chrome"
+
 # Php shell vars
 if type -q php
-  set -x PATH $HOME/.composer/vendor/bin $PATH
-  alias composer='php $HOME/.composer/composer'
-  alias artisan='php artisan'
+    set -x PATH $HOME/.composer/vendor/bin $PATH
+    alias composer='php $HOME/.composer/composer'
+    alias artisan='php artisan'
 end
 
 # Go shell vars
 if type -q go
-  set -Ux GOPATH $HOME/go
-  set -x PATH $GOPATH/bin $PATH
-  set -Ux GOPRIVATE bitbucket.org/teamscript
+    set -Ux GOPATH $HOME/go
+    set -x PATH $GOPATH/bin $PATH
+    set -Ux GOPRIVATE bitbucket.org/teamscript
 end
 
 # Rust and Cargo shell vars
 set -x PATH $HOME/.cargo/bin $PATH
 if type -q cargo
-  set -Ux CARGO_NAME "mjarkk"
-  set -Ux CARGO_EMAIL "mkopenga@gmail.com"
+    set -Ux CARGO_NAME mjarkk
+    set -Ux CARGO_EMAIL "mkopenga@gmail.com"
 end
 
 if type -q zoxide
-  zoxide init fish | source
+    zoxide init fish | source
 end
 
 # On some linux distro's there is a popup for filling in the gpg passphrase and i don't like that
 set -Ux GPG_AGENT_INFO ""
 
 # Git config, so i don't have to setup this every time
-set -Ux GIT_AUTHOR_NAME "mjarkk"
+set -Ux GIT_AUTHOR_NAME mjarkk
 set -Ux GIT_AUTHOR_EMAIL "mkopenga@gmail.com"
 
 # Give node a shitload of memory just so angular can do it's "special" things
 set -Ux NODE_OPTIONS "--max-old-space-size=4096"
 
 # Disable analytics for some programs
-set -Ux NG_CLI_ANALYTICS "false" # Angular
-set -Ux NEXT_TELEMETRY_DISABLED "1" # Next.js
+set -Ux NG_CLI_ANALYTICS false # Angular
+set -Ux NEXT_TELEMETRY_DISABLED 1 # Next.js
 
 # If thefuck is installed set it up automaticly
 if type -q thefuck
-  thefuck --alias | source
+    thefuck --alias | source
 end
 
 # Use nvim everywhere
 if type -q nvim
-  set -Ux EDITOR "nvim"
-  set -Ux GIT_EDITOR "nvim"
-  alias vi="nvim"
-  alias vim="nvim"
+    set -Ux EDITOR nvim
+    set -Ux GIT_EDITOR nvim
+    alias vi="nvim"
+    alias vim="nvim"
 else
-  set -Ux EDITOR "nano"
+    set -Ux EDITOR nano
 end
 
 # some distros install the open source edition of vs-code, bind that to code if that's the case
 if type -q code-oss
-  alias code='code-oss'
+    alias code='code-oss'
 end
 
 # If discord won't die
@@ -77,11 +81,11 @@ alias p='pwd'
 alias lsd='ls -D'
 
 function open
-  if uname | grep 'Darwin' > /dev/null
-    /usr/bin/open $argv
-  else
-    xdg-open $argv
-  end
+    if uname | grep Darwin >/dev/null
+        /usr/bin/open $argv
+    else
+        xdg-open $argv
+    end
 end
 
 # Shortcut to untar something
@@ -104,17 +108,17 @@ alias ips='ip addr | grep "\([0-9]\{1,3\}\.\)\{3\}\([0-9]\{1,3\}\)" | sed -e "s/
 
 # Bind cat to bat -p, this makes using cat sooo much better
 if type -q bat
-  alias cat='bat -p'
+    alias cat='bat -p'
 end
 
 # Bind ls to exa, exa has some nice things that make using ls just a bit better
 if type -q exa
-  alias ls='exa'
+    alias ls='exa'
 end
 
 # Set a docker alias to podman as most docker commands work with podman
 if type -q podman
-  alias docker='podman'
+    alias docker='podman'
 end
 
 # Aliases for systemctl
@@ -122,15 +126,15 @@ alias startDocker='ss start docker'
 alias startCodeServer='ss start --now code-server@$USER'
 
 # Macos spesific shell vars
-if uname | grep 'Darwin' > /dev/null
-  set -x PATH /usr/local/opt/php@7.4/bin $PATH
-  set -x PATH /usr/local/opt/rabbitmq/sbin $PATH
+if uname | grep Darwin >/dev/null
+    set -x PATH /usr/local/opt/php@7.4/bin $PATH
+    set -x PATH /usr/local/opt/rabbitmq/sbin $PATH
 
-  alias startMongo='brew services start mongodb-community@5.0'
-  alias stopMongo='brew services stop mongodb-community'
-  alias startRedis='redis-server /usr/local/etc/redis.conf'
-  alias startPostgres='brew services run postgresql'
-  alias stopPostgres='brew services restart postgresql'
+    alias startMongo='brew services start mongodb-community@5.0'
+    alias stopMongo='brew services stop mongodb-community'
+    alias startRedis='redis-server /usr/local/etc/redis.conf'
+    alias startPostgres='brew services run postgresql'
+    alias stopPostgres='brew services restart postgresql'
 end
 
 # Cross distro bindings to make system updates a bit faster
@@ -142,61 +146,61 @@ alias yayup='yay -Syuu --noeditmenu --answerdiff None --answeredit None --answer
 alias dnfup='sudo dnf update -y'
 alias xbpsup='sudo xbps-install -Syu'
 function aptup
-  sudo apt update -y
-  sudo apt upgrade -y
-  sudo apt autoremove -y
+    sudo apt update -y
+    sudo apt upgrade -y
+    sudo apt autoremove -y
 end
 function brewup
-  brew update
-  brew upgrade
+    brew update
+    brew upgrade
 end
 
 function up
-  if uname | grep 'Darwin' > /dev/null
-    # Macos
-    brewup
-  else
-    # This is a linux distro
-    set ID_LIKE (grep "^ID_LIKE=" /etc/os-release | sed 's/ID_LIKE=//' | sed 's/"//g')
-    set ID (grep "^ID=" /etc/os-release | sed 's/ID=//' | sed 's/"//g')
-
-    if [ $ID = 'arch' ] || [ $ID_LIKE = 'arch' ]
-      if type -q yay
-        yayup
-      else
-        pacup
-      end
-    else if [ $ID_LIKE = 'ubuntu debian' ] || [ $ID_LIKE = 'debian' ] || [ $ID_LIKE = 'ubuntu' ] ||  [ $ID = 'ubuntu' ] || [ $ID = 'debian' ]
-      aptup
-    else if [ $ID = 'solus' ] || [ $ID_LIKE = 'solus' ]
-      eoup
-    else if [ $ID = 'void' ] || [ $ID_LIKE = 'void' ]
-      xbpsup
-    else if [ $ID = 'fedora' ] || [ $ID_LIKE = 'fedora' ]
-      dnfup
+    if uname | grep Darwin >/dev/null
+        # Macos
+        brewup
     else
-      echo 'Unknown distro: $ID'
-    end
-  end
+        # This is a linux distro
+        set ID_LIKE (grep "^ID_LIKE=" /etc/os-release | sed 's/ID_LIKE=//' | sed 's/"//g')
+        set ID (grep "^ID=" /etc/os-release | sed 's/ID=//' | sed 's/"//g')
 
-  # Also check for flatpak and rust updates if installed
-  if type -q flatpak
-    flatup
-  end
-  if type -q rustup
-    rusup
-  end
-  if type -q flutter
-    flutter upgrade
-  end
+        if [ $ID = arch ] || [ $ID_LIKE = arch ]
+            if type -q yay
+                yayup
+            else
+                pacup
+            end
+        else if [ $ID_LIKE = 'ubuntu debian' ] || [ $ID_LIKE = debian ] || [ $ID_LIKE = ubuntu ] || [ $ID = ubuntu ] || [ $ID = debian ]
+            aptup
+        else if [ $ID = solus ] || [ $ID_LIKE = solus ]
+            eoup
+        else if [ $ID = void ] || [ $ID_LIKE = void ]
+            xbpsup
+        else if [ $ID = fedora ] || [ $ID_LIKE = fedora ]
+            dnfup
+        else
+            echo 'Unknown distro: $ID'
+        end
+    end
+
+    # Also check for flatpak and rust updates if installed
+    if type -q flatpak
+        flatup
+    end
+    if type -q rustup
+        rusup
+    end
+    if type -q flutter
+        flutter upgrade
+    end
 end
 
 function darkTheme
-  set -Ux BAT_THEME Monokai Extended
+    set -Ux BAT_THEME Monokai Extended
 end
 
 function lightTheme
-  set -Ux BAT_THEME Monokai Extended Light
+    set -Ux BAT_THEME Monokai Extended Light
 end
 
 # By default use the dark theme
@@ -213,11 +217,11 @@ alias startMinio='docker run -e "MINIO_ACCESS_KEY=BByNC8gT7WEaT5QOJLHhwBywds8e4i
 
 # Run minio locally
 function startMinioLocal
-  export MINIO_ACCESS_KEY="BByNC8gT7WEaT5QOJLHhwBywds8e4iSaZSrwduhsm"
-  export MINIO_SECRET_KEY="BcJKJBTxw8YLg9ouEETQXywTCZkxeXz28GYmAYW7R"
+    export MINIO_ACCESS_KEY="BByNC8gT7WEaT5QOJLHhwBywds8e4iSaZSrwduhsm"
+    export MINIO_SECRET_KEY="BcJKJBTxw8YLg9ouEETQXywTCZkxeXz28GYmAYW7R"
 
-  export MINIO_ROOT_USER="BByNC8gT7WEaT5QOJLHhwBywds8e4iSaZSrwduhsm"
-  export MINIO_ROOT_PASSWORD="BcJKJBTxw8YLg9ouEETQXywTCZkxeXz28GYmAYW7R"
+    export MINIO_ROOT_USER="BByNC8gT7WEaT5QOJLHhwBywds8e4iSaZSrwduhsm"
+    export MINIO_ROOT_PASSWORD="BcJKJBTxw8YLg9ouEETQXywTCZkxeXz28GYmAYW7R"
 
-  minio server ~/.minio_data
+    minio server ~/.minio_data
 end
--- setup_vscode.fish
+++ setup_vscode.fish
@@ -1,8 +1,8 @@
 #!/usr/bin/fish
 
 if type -q code
-  cp vscode/keybindings.json ~/.config/Code/User/keybindings.json
-  cp vscode/settings.json ~/.config/Code/User/settings.json
+    cp vscode/keybindings.json ~/.config/Code/User/keybindings.json
+    cp vscode/settings.json ~/.config/Code/User/settings.json
 else
-  echo "Code command not defined"
+    echo "Code command not defined"
 end
